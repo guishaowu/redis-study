3db9630c3322c77b43fddec3cded0d903bcd62ea
guishaowu <guisw@chinanetcenter.com>
2017-07-06 08:45:24 +0800

    字典

--- src/ae.h
+++ src/ae.h
@@ -53,28 +53,43 @@
 /* Macros */
 #define AE_NOTUSED(V) ((void) V)
 
+/**
+ * 事件处理Loop
+ */
 struct aeEventLoop;
 
 /* Types and data structures */
+/**
+ * 文件事件处理方法
+ */
 typedef void aeFileProc(struct aeEventLoop *eventLoop, int fd, void *clientData, int mask);
+/**
+ * 时间事件处理方法
+ */
 typedef int aeTimeProc(struct aeEventLoop *eventLoop, long long id, void *clientData);
 typedef void aeEventFinalizerProc(struct aeEventLoop *eventLoop, void *clientData);
 typedef void aeBeforeSleepProc(struct aeEventLoop *eventLoop);
 
 /* File event structure */
+/**
+ * 文件事件数据结构
+ */
 typedef struct aeFileEvent {
-    int mask; /* one of AE_(READABLE|WRITABLE) */
+    int mask; /* 事件类型， 可读事件或者可写事件 one of AE_(READABLE|WRITABLE) */
     aeFileProc *rfileProc;  // 文件事件处理函数
     aeFileProc *wfileProc;  // 文件事件处理函数
     void *clientData;       // 事件数据
 } aeFileEvent;
 
 /* Time event structure */
+/**
+ * 时间事件数据结构
+ */
 typedef struct aeTimeEvent {
     long long id; /* time event identifier.  AE_DELETED_EVENT_ID 删除标识*/
-    long when_sec; /* seconds */
-    long when_ms; /* milliseconds */
-    aeTimeProc *timeProc;  //时间事件处理
+    long when_sec; /* seconds 事件应该被处理的时间 秒*/
+    long when_ms; /* milliseconds* 事件应该被处理的时间 毫秒 */
+    aeTimeProc *timeProc;  //时间事件处理函数
     aeEventFinalizerProc *finalizerProc;
     void *clientData;    //事件数据
     struct aeTimeEvent *next;  // 下一个时间事件
@@ -87,6 +102,9 @@ typedef struct aeFiredEvent {
 } aeFiredEvent;
 
 /* State of an event based program */
+/**
+ * 事件处理Loop的数据结构
+ */
 typedef struct aeEventLoop {
     int maxfd;   /* highest file descriptor currently registered */
     int setsize; /* max number of file descriptors tracked */
@@ -96,21 +114,34 @@ typedef struct aeEventLoop {
     aeFiredEvent *fired; /* Fired events */
     aeTimeEvent *timeEventHead; // 时间事件列表
     int stop;      // 停止标识
-    void *apidata; /* This is used for polling API specific data */
+    void *apidata; /*使用的事件注册，事件触发的api This is used for polling API specific data */
     aeBeforeSleepProc *beforesleep;
     aeBeforeSleepProc *aftersleep;
 } aeEventLoop;
 
 /* Prototypes */
 // 创建一个大小 setsize的EventLoop
+/**
+ * 创建事件处理Loop
+ * @param setsize  文件事件的最大数量
+ * @return
+ */
 aeEventLoop *aeCreateEventLoop(int setsize);
+/**
+ * 删除事件处理Loop
+ * @param eventLoop
+ */
 void aeDeleteEventLoop(aeEventLoop *eventLoop);
-// 设置停止标识
+/**
+ * 设置事件处理Loop停止标识
+ * @param eventLoop 事件处理Loop
+ * @return
+ */
 void aeStop(aeEventLoop *eventLoop);
 //
 /**
  * 创建文件事件
- * @param eventLoop 事件处理框架
+ * @param eventLoop 事件处理Loop
  * @param fd 文件描述符
  * @param mask 事件类型
  * @param proc 事件处理函数
@@ -119,7 +150,12 @@ void aeStop(aeEventLoop *eventLoop);
  */
 int aeCreateFileEvent(aeEventLoop *eventLoop, int fd, int mask,
         aeFileProc *proc, void *clientData);
-//删除文件事件
+/**
+ * 删除文件事件
+ * @param eventLoop 事件处理Loop
+ * @param fd 文件描述符
+ * @param mask 事件类型（可读或者可写）
+ */
 void aeDeleteFileEvent(aeEventLoop *eventLoop, int fd, int mask);
 
 int aeGetFileEvents(aeEventLoop *eventLoop, int fd);
@@ -132,7 +168,10 @@ int aeDeleteTimeEvent(aeEventLoop *eventLoop, long long id);
 //出来事件
 int aeProcessEvents(aeEventLoop *eventLoop, int flags);
 int aeWait(int fd, int mask, long long milliseconds);
-// 事件循环处理，直到设置stop标识
+/**
+ *  事件循环处理，直到设置stop标识
+ * @param eventLoop 事件处理Loop
+ */
 void aeMain(aeEventLoop *eventLoop);
 char *aeGetApiName(void);
 void aeSetBeforeSleepProc(aeEventLoop *eventLoop, aeBeforeSleepProc *beforesleep);
--- src/dict.h
+++ src/dict.h
@@ -45,6 +45,9 @@
 #define DICT_NOTUSED(V) ((void) V)
 
 // key-value  next解决hash值冲突
+/**
+ * 键值对
+ */
 typedef struct dictEntry {
     void *key;
     union {
@@ -56,6 +59,10 @@ typedef struct dictEntry {
     struct dictEntry *next;
 } dictEntry;
 // key hash,复制,比较,释放方法  value 复制，释放方法
+/**
+ * 键值对类型， 键的hash方法，复制，比较，释放方法
+ * 值的复制，释放方法
+ */
 typedef struct dictType {
     uint64_t (*hashFunction)(const void *key);
     void *(*keyDup)(void *privdata, const void *key);
@@ -67,13 +74,19 @@ typedef struct dictType {
 
 /* This is our hash table structure. Every dictionary has two of this as we
  * implement incremental rehashing, for the old to the new table. */
+/**
+ * 字典表 HashTable
+ */
 typedef struct dictht {
-    dictEntry **table;
-    unsigned long size;
-    unsigned long sizemask;
-    unsigned long used;
+    dictEntry **table;  // 键值对数组
+    unsigned long size; // 大小
+    unsigned long sizemask; // size -1
+    unsigned long used; // 已使用
 } dictht;
 
+/**
+ * 字典，包含两个字典表，方便做rehash, 由于字典表大小的限制
+ */
 typedef struct dict {
     dictType *type;
     void *privdata;
@@ -86,6 +99,9 @@ typedef struct dict {
  * dictAdd, dictFind, and other functions against the dictionary even while
  * iterating. Otherwise it is a non safe iterator, and only dictNext()
  * should be called while iterating. */
+/**
+ * 字典迭代器
+ */
 typedef struct dictIterator {
     dict *d;
     long index;
@@ -94,7 +110,9 @@ typedef struct dictIterator {
     /* unsafe iterator fingerprint for misuse detection. */
     long long fingerprint;
 } dictIterator;
-
+/**
+ *
+ */
 typedef void (dictScanFunction)(void *privdata, const dictEntry *de);
 typedef void (dictScanBucketFunction)(void *privdata, dictEntry **bucketref);
 
@@ -102,10 +120,15 @@ typedef void (dictScanBucketFunction)(void *privdata, dictEntry **bucketref);
 #define DICT_HT_INITIAL_SIZE     4
 
 /* ------------------------------- Macros ------------------------------------*/
+/**
+ * 释放键值对的值
+ */
 #define dictFreeVal(d, entry) \
     if ((d)->type->valDestructor) \
         (d)->type->valDestructor((d)->privdata, (entry)->v.val)
-
+/**
+ * 设置键值对的值
+ */
 #define dictSetVal(d, entry, _val_) do { \
     if ((d)->type->valDup) \
         (entry)->v.val = (d)->type->valDup((d)->privdata, _val_); \
@@ -113,19 +136,33 @@ typedef void (dictScanBucketFunction)(void *privdata, dictEntry **bucketref);
         (entry)->v.val = (_val_); \
 } while(0)
 
+/**
+ * 设置键值对有符号整型值
+ */
 #define dictSetSignedIntegerVal(entry, _val_) \
     do { (entry)->v.s64 = _val_; } while(0)
 
+/**
+ * 设置键值对的无符号整型值
+ */
 #define dictSetUnsignedIntegerVal(entry, _val_) \
     do { (entry)->v.u64 = _val_; } while(0)
-
+/**
+ * 设置键值对的double值
+ */
 #define dictSetDoubleVal(entry, _val_) \
     do { (entry)->v.d = _val_; } while(0)
 
+/**
+ * 释放键值对的键
+ */
 #define dictFreeKey(d, entry) \
     if ((d)->type->keyDestructor) \
         (d)->type->keyDestructor((d)->privdata, (entry)->key)
 
+/**
+ * 设置键值对的键
+ */
 #define dictSetKey(d, entry, _key_) do { \
     if ((d)->type->keyDup) \
         (entry)->key = (d)->type->keyDup((d)->privdata, _key_); \
@@ -133,82 +170,245 @@ typedef void (dictScanBucketFunction)(void *privdata, dictEntry **bucketref);
         (entry)->key = (_key_); \
 } while(0)
 
+/**
+ * 比较两个key
+ */
 #define dictCompareKeys(d, key1, key2) \
     (((d)->type->keyCompare) ? \
         (d)->type->keyCompare((d)->privdata, key1, key2) : \
         (key1) == (key2))
-
+/**
+ * key 在字典中的hash值
+ */
 #define dictHashKey(d, key) (d)->type->hashFunction(key)
+/**
+ * 获取键值对的键值
+ */
 #define dictGetKey(he) ((he)->key)
+/**
+ * 获取键值对的值
+ */
 #define dictGetVal(he) ((he)->v.val)
+/**
+ * 获取键值对的有符号整型值
+ */
 #define dictGetSignedIntegerVal(he) ((he)->v.s64)
+/**
+ * 获取键值对无符号整型值
+ */
 #define dictGetUnsignedIntegerVal(he) ((he)->v.u64)
+/**
+ *  获取键值对的double值
+ */
 #define dictGetDoubleVal(he) ((he)->v.d)
+/**
+ * 字典的大小
+ */
 #define dictSlots(d) ((d)->ht[0].size+(d)->ht[1].size)
+/**
+ * 字典已使用的大小
+ */
 #define dictSize(d) ((d)->ht[0].used+(d)->ht[1].used)
+/**
+ * 字典是否在rehash中
+ */
 #define dictIsRehashing(d) ((d)->rehashidx != -1)
 
 /* API */
-// 创建字典，初始化
+/**
+ * 创建字典，初始化
+ */
 dict *dictCreate(dictType *type, void *privDataPtr);
 //创建字典的hashTable
 int dictExpand(dict *d, unsigned long size);
-/*添加一个键值对到字典
-调用dictAddRaw，如果键已经存在，返回error
+/**
 */
+/**
+ *  * 添加一个键值对到字典
+调用dictAddRaw，如果键已经存在，返回error
+ * @param d 字典
+ * @param key  键
+ * @param val  值
+ * @return
+ */
 int dictAdd(dict *d, void *key, void *val);
-/*
-添加一个键到字典，返回对应的键值对（没有设置值）
+/**
+ * 添加一个键到字典，返回对应的键值对（没有设置值）
 如果正在rehash, 执行一次移动元素操作
 如果键已经存在，返回NULL
-*/
+ * @param d  字典
+ * @param key  键
+ * @param existing 保存key存在时的键值对
+ * @return
+ */
 dictEntry *dictAddRaw(dict *d, void *key, dictEntry **existing);
-/*
-返回对应键的键值对，如果没有，就讲键添加到字典
-*/
+/**
+ * 返回对应键的键值对，如果没有，就讲键添加到字典
+ * @param d  字典
+ * @param key 键
+ * @return
+ */
 dictEntry *dictAddOrFind(dict *d, void *key);
-/*
-键不存在，添加键值对，键存在，更新值
-*/
+/**
+ * 键不存在，添加键值对，键存在，更新值
+ * @param d 字典
+ * @param key  键
+ * @param val 值
+ * @return
+ */
 int dictReplace(dict *d, void *key, void *val);
-// 删除元素，如果rehash,移动一次
+/**
+ * 删除元素，如果rehash,移动一次
+* @param d 字典
+ * @param key 键
+ * @return 成功或者失败
+ */
 int dictDelete(dict *d, const void *key);
-//删除元素，不释放，如果key不存在，返回Null
+/**
+ * 删除元素，不释放，如果key不存在，返回Null
+ * @param ht 字典
+ * @param key 键
+ * @return 删除的键值对
+ */
 dictEntry *dictUnlink(dict *ht, const void *key);
-// 释放上一个函数删除的元素
+/**
+ * 释放上一函数的删除的元素
+ * @param d 字典
+ * @param he 兼职对
+ * @return
+ */
 void dictFreeUnlinkedEntry(dict *d, dictEntry *he);
-//释放字典
+/**
+ * 释放字典
+ * @param d 字典
+ * @return
+ */
 void dictRelease(dict *d);
-//字典中查找key，对应的键值对，没有返回null,如果rehash，移动一次
+/**
+ *  字典中查找key，对应的键值对，没有返回null,如果rehash，移动一次
+ * @param d 字典
+ * @param key  键
+ * @return 查找结果
+ */
 dictEntry * dictFind(dict *d, const void *key);
-//返回字典中对应键的值
+/**
+ * 返回字典中对应键的值
+ * @param d 字典
+ * @param key  键
+ * @return
+ */
 void *dictFetchValue(dict *d, const void *key);
-//回收空间
+/**
+ * 回收空间，根据已经使用的，使用最小的size
+ * @param d
+ * @return
+ */
 int dictResize(dict *d);
 //迭代器
 dictIterator *dictGetIterator(dict *d);
 dictIterator *dictGetSafeIterator(dict *d);
-// 下一个元素
+/**
+ * 迭代器 迭代元素
+ * @param iter
+ * @return
+ */
 dictEntry *dictNext(dictIterator *iter);
+/**
+ * 释放迭代器
+ * @param iter
+ */
 void dictReleaseIterator(dictIterator *iter);
-// 获取一个随机元素
+/**
+ * 获取一个随机元素
+ * @param d
+ * @return
+ */
 dictEntry *dictGetRandomKey(dict *d);
-//获取count个随机元素，不重复，不保证一定有count个结果
+/**
+ * 获取count个随机元素，不重复，不保证一定有count个结果
+ * @param d
+ * @param des
+ * @param count
+ * @return
+ */
 unsigned int dictGetSomeKeys(dict *d, dictEntry **des, unsigned int count);
+/**
+ * 返回字典的统计信息，元素数量等信息
+ * @param buf 存储统计信息
+ * @param bufsize
+ * @param d
+ */
 void dictGetStats(char *buf, size_t bufsize, dict *d);
+/**
+ * 生成hash 值
+ * @param key
+ * @param len
+ * @return
+ */
 uint64_t dictGenHashFunction(const void *key, int len);
 uint64_t dictGenCaseHashFunction(const unsigned char *buf, int len);
+/**
+ * 清空字典
+ * @param d
+ * @param callback 当hashtable的索引与65535为0时调用callback参数d.privatedata
+ */
 void dictEmpty(dict *d, void(callback)(void*));
+/**
+ * 打开resize标识
+ */
 void dictEnableResize(void);
+/**
+ * 关闭resize标识
+ */
 void dictDisableResize(void);
-// 执行n次rehash的移动元素操作
+/**
+ * 执行n次rehash的移动元素操作
+ * @param d
+ * @param n
+ * @return
+ */
 int dictRehash(dict *d, int n);
+/**
+ * 执行ms毫秒rehash的移动元素操作
+ * @param d
+ * @param ms
+ * @return
+ */
 int dictRehashMilliseconds(dict *d, int ms);
+/**
+ * 设置hash 函数的种子
+ * @param seed
+ */
 void dictSetHashFunctionSeed(uint8_t *seed);
+/**
+ * 获取hash函数的种子
+ * @return
+ */
 uint8_t *dictGetHashFunctionSeed(void);
+/**
+ * scan字典
+ * @param d
+ * @param v
+ * @param fn
+ * @param bucketfn
+ * @param privdata
+ * @return
+ */
 unsigned long dictScan(dict *d, unsigned long v, dictScanFunction *fn, dictScanBucketFunction *bucketfn, void *privdata);
-//key对应的hash值
+/**
+ * key对应的hash值
+ * @param d 字典
+ * @param key 键
+ * @return
+ */
 unsigned int dictGetHash(dict *d, const void *key);
+/**
+ * 根据key指针和hash值，找到对应的键值对
+ * @param d
+ * @param oldptr key指针
+ * @param hash 
+ * @return
+ */
 dictEntry **dictFindEntryRefByPtrAndHash(dict *d, const void *oldptr, unsigned int hash);
 
 /* Hash table types */
